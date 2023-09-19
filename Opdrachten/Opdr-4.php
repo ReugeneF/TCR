cafbeaf3023ce98e9faaa8f8fe55495a895ad6e6
Reugene Forst <reugene@hellohix.com>
2023-09-19 15:40:37 +0200

    Opdr4 - af

---
+++ Opdrachten/Opdr-4.php
@@ -0,0 +1,21 @@
+<?php
+
+    class candyshop{
+        public function __construct(public $titel, public $category, public $price)
+        {
+            $this->titel = strtolower($titel);
+            $this->category = strtoupper($category);
+        }
+
+        function showSalesPrice(){
+           return number_format($this->price *1.09, 2);
+        }
+
+    }
+
+    $sweetyBag = new candyshop("Candy Bag", "Candy" ,10);
+    echo $sweetyBag->titel."<br>Category: ";
+    echo $sweetyBag->category."<br>price: ";
+    echo $sweetyBag->price." inc BTW: ";
+    echo $sweetyBag->showSalesPrice()."";
+
